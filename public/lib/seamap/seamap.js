6daa2edf171b64dc6e9fe4281646266d0cfb2579
wokaiser <Wolfgang.Kaiser3012@gmx.de>
2013-12-07 12:42:27 +0100

    Implemented long touch/click to delete route point

--- public/lib/seamap/seamap.js
+++ public/lib/seamap/seamap.js
@@ -349,6 +349,27 @@
                         break;
                 }
             });
+            //long press
+            /*new LongPress(map, 300);
+            google.maps.event.addListener(map, 'longpress', function(event) {
+                switch(state) {
+                    case States.NORMAL: 
+                        hideContextMenu();
+                        hideCrosshairMarker(crosshairMarker);
+                        showCrosshairMarker(event.latLng);
+                        break;
+                        
+                    case States.ROUTE:
+                        hideCrosshairMarker(crosshairMarker);
+                        showCrosshairMarker(event.latLng);
+                        showContextMenu(event.latLng, ContextMenuTypes.DEFAULT, crosshairMarker);
+                        break;
+                        
+                    case States.DISTANCE:
+                        handleExitDistanceRouteCreation();
+                        break;
+                }
+            });*/
             // left click
             google.maps.event.addListener(map, 'click', function(event) {
                 hideCrosshairMarker(crosshairMarker);
@@ -1057,6 +1078,12 @@
                     $this.notify("remove");
                 });
                 
+                new LongPress(marker, 500);
+                google.maps.event.addListener(marker, 'longpress', function(event) {
+                    $this.removeMarker(marker);
+                    $this.notify("remove");
+                });
+
                 google.maps.event.addListener(marker, 'click', function(event) {
                     $this.notify("click");
                 });
@@ -1067,6 +1094,44 @@
             return marker;
         }
         
+        /**
+        * *********************************************************************************
+        * Recognizes a long click / long touch
+        * *********************************************************************************
+        */
+        function LongPress(map, length) {
+            this.length_ = length;
+            var me = this;
+            me.map_ = map;
+            me.timeoutId_ = null;
+            google.maps.event.addListener(map, 'mousedown', function(e) {
+                me.onMouseDown_(e);
+            });
+            google.maps.event.addListener(map, 'mouseup', function(e) {
+                me.onMouseUp_(e);
+            });
+            google.maps.event.addListener(map, 'drag', function(e) {
+                me.onMapDrag_(e);
+            });
+        };
+
+        LongPress.prototype.onMouseUp_ = function(e) {
+            clearTimeout(this.timeoutId_);
+        };
+
+        LongPress.prototype.onMouseDown_ = function(e) {
+            clearTimeout(this.timeoutId_);
+            var map = this.map_;
+            var event = e;
+            this.timeoutId_ = setTimeout(function() {
+                google.maps.event.trigger(map, 'longpress', event);
+            }, this.length_);
+        };
+
+        LongPress.prototype.onMapDrag_ = function(e) {
+            clearTimeout(this.timeoutId_);
+        };
+
         /**
         * *********************************************************************************
         * Removes a marker from the route.
