fda8154a7e513c4ebbe62f1d4f815286d5971e1c
deparlak <deparlak@googlemail.com>
2013-12-18 17:15:44 +0100

    - added history for searchPosition.js

--- public/js/app/callbacks/tools/searchPosition.js
+++ public/js/app/callbacks/tools/searchPosition.js
@@ -7,6 +7,7 @@
  
 $(document).ready(function() {    
     var lastSearch = {};
+    var history = {};
     var active = "#SearchPlaces";
     var method = {};
     var service = new google.maps.places.PlacesService(map.getGoogleMapsHandle());
@@ -38,11 +39,100 @@ $(document).ready(function() {
         return string;
     });
     
+    function addToHistory(search) {
+        //history exist for this element
+        if (history[active] === undefined){
+            history[active] = [];
+        }
+        //element already in history
+        if (-1 != history[active].indexOf(search)) {
+            return;
+        }
+        //history over max
+        if (history[active].length > 20) {
+            history[active].pop();
+        }
+        //set new history entry to begin to show the latest searched things first.
+        history[active].unshift(search);
+    }
+    
+    function removeFromHistory() {
+        //remove the last insert entry from the history.
+        if (!(history[active] === undefined)){
+            history[active].shift();
+        }
+    }
+    
+    function LatLngToDecimal(string) {
+        var obj = {};
+        if (24 != string.length) {
+            obj.error = "Lat Lng input has a not correct length.";
+            return obj;
+        }
+        var latDegree = parseInt(string.substring(0, 2));
+        var latMin = parseInt(string.substring(3, 5));
+        var latSec = parseInt(string.substring(6, 8));
+        var latDir = string.substring(10, 11);
+        var lonDegree = parseInt(string.substring(12, 15));
+        var lonMin = parseInt(string.substring(16, 18));
+        var lonSec = parseInt(string.substring(19, 21));
+        var lonDir = string.substring(23, 24);
+        
+        if (latDegree < 0 || latDegree > 90 || isNaN(latDegree)) {
+            obj.error = "Lat degree should be in range of 0 to 90";
+            return obj;
+        }
+        if (lonDegree < 0 || lonDegree > 90 || isNaN(lonDegree)) {
+            obj.error = "Lon degree should be in range of 0 to 180";
+            return obj;
+        }
+        if (latMin < 0 || latMin > 60 || isNaN(latMin)) {
+            obj.error = "Lat min should be in range of 0 to 60";
+            return obj;
+        }
+        if (lonMin < 0 || lonMin > 60 || isNaN(lonMin)) {
+            obj.error = "Lon min should be in range of 0 to 60";
+            return obj;
+        }
+        if (latSec < 0 || latSec > 60 || isNaN(latSec)) {
+            obj.error = "Lat sec should be in range of 0 to 60";
+            return obj;
+        }
+        if (lonSec < 0 || lonSec > 60 || isNaN(lonSec)) {
+            obj.error = "Lon sec should be in range of 0 to 60";
+            return obj;
+        }
+        if (latDir != 'N' && latDir != 'S') {
+            obj.error = "Lat Direction has to be 'N' or 'S'.";
+            return obj;
+        }
+        if (lonDir != 'W' && lonDir != 'E') {
+            obj.error = "Lon Direction has to be 'W' or 'E'.";
+            return obj;
+        }
+        obj.lat = latDegree + latMin * 0.6 + latSec * 0.36;
+        obj.lon = lonDegree + lonMin * 0.6 + lonSec * 0.36;
+        if ('W' == latDir) {
+            obj.lat = -1 * obj.lat;
+        }
+        if ('S' == latDir) {
+            obj.lon = -1 * obj.lon;
+        }  
+        return obj;
+    }
+    
+    
     function renderCallback(div, results, status) {
         var render = {};
         render.status = {};
         render.status[status] = true;
         render[div.substring(1,99)] = results;
+        
+        //query was not ok, so remove the searched jquery from the history
+        if (!render.status.OK) {
+            removeFromHistory();
+        }
+        console.log(history);
         $(div).html(templateSearchPlaces(render));
     }
     
@@ -81,7 +171,15 @@ $(document).ready(function() {
             $(active).html(templateSearchPlaces({status : {SEARCHING_COORDINATES_INCOMPLETE : true}}));
             return;
         }
-        var latlng = new google.maps.LatLng(40.730885,-73.997383);
+        var obj = LatLngToDecimal(search);
+        //check if an error occurred while parsing
+        if (obj.error) {
+            $(active).html(templateSearchPlaces(obj));
+            return;
+        }
+        console.log(search);
+        console.log(obj);
+        var latlng = new google.maps.LatLng(obj.lat, obj.lon);
         geocoder.geocode({'latLng': latlng},SearchCoordinatesCallback);
     };
 
@@ -101,10 +199,10 @@ $(document).ready(function() {
         } else {
             $('#search-searchPosition').inputmask({mask: "99°99.99' c 999°99.99' d"});
         }        
-        $('#search-searchPosition').val(lastSearch[active]);
         /* unfocus and focus because the search input was modified before */
-        $('#search-searchPosition').blur();
-        $('#search-searchPosition').focus();
+        //$('#search-searchPosition').blur();
+        //$('#search-searchPosition').focus();
+        $('#search-searchPosition').val(lastSearch[active]);
     });
     
     $("#search-searchPosition").keydown( function(event) {
@@ -113,8 +211,9 @@ $(document).ready(function() {
             var search = "";
             search += $('#search-searchPosition').val();
             if (search.length > 0) {
+                addToHistory(search);
                 method[active](search);
-                $('#search-searchPosition').blur();
+//                $('#search-searchPosition').blur();
             }
         //if we search not for marks, we will display a message that the search is in action.
         } else if (active == "#SearchPlaces") {
