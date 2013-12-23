1f6a769852172481e76250b518c633dcb8491ef4
deparlak <deparlak@googlemail.com>
2013-12-23 21:01:40 +0100

    - updated remove functionality for marks/routes/tracks

--- public/js/app/callbacks/menu/marksRoutesTracks.js
+++ public/js/app/callbacks/menu/marksRoutesTracks.js
@@ -8,12 +8,15 @@
 $(document).ready(function() {    
     var lastSearch = {};
     var active = "#marks";
-    var states = {normal : 0, edit : 1, add : 2, remove : 3};
+    var states = {normal : 0, edit : 1, remove : 2};
     var state = states.normal;
     var removeElements = {};
 
     /* calling initState will clear the list of items which shall be removed and set the state back to normal */
     initState = function (){
+        for (var i in removeElements) {
+            removeElements[i].removeClass('remove');
+        }
         removeElements = {};
         state = states.normal;
     };
@@ -30,16 +33,14 @@ $(document).ready(function() {
     
     removeSelection = function () {
         for (var i in removeElements) {
-            console.log("remove "+i);
+            console.log("TODO call remove method in seamap.js for "+active+" "+removeElements[i].data('id'));
+            removeElements[i].remove();
         }
         state = states.normal;
     };
     
     search = function(inElement, search) {
-        console.log(inElement);
-        console.log(search);
         $(inElement+" li").each(function(index) {
-            console.log($(this));
             //does not match search criterium, so hide this element
             if (-1 == $(this).text().indexOf(search)) {
                 $(this).removeClass("visible").addClass("hidden");
@@ -58,27 +59,37 @@ $(document).ready(function() {
         state = states.edit;
     });
     
-    menu.addCallback('leftclick', 'marksRoutesTracksAdd', function (self) {
-        state = states.add;
+    menu.addCallback('leftclick', 'marksRoutesTracksRemoveOk', function (self) {
+        removeSelection();
+        state = states.normal;
+        $('#menu-marksRoutesTracksRemove-footer').removeClass('visible').addClass('hidden');
+        $('#menu-marksRoutesTracks-footer').removeClass('hidden').addClass('visible');
+    });
+    
+    menu.addCallback('leftclick', 'marksRoutesTracksRemoveCancel', function (self) {
+        initState();
+        $('#menu-marksRoutesTracksRemove-footer').removeClass('visible').addClass('hidden');
+        $('#menu-marksRoutesTracks-footer').removeClass('hidden').addClass('visible');
     });
 
     menu.addCallback('leftclick', 'marksRoutesTracksRemove', function (self) {
-        if (state == states.remove) {
-            removeSelection();
-        } else {
-            state = states.remove;
-        }
+        $('#menu-marksRoutesTracks-footer').removeClass('visible').addClass('hidden');
+        $('#menu-marksRoutesTracksRemove-footer').removeClass('hidden').addClass('visible');
+        state = states.remove;    
     });
     
     menu.addCallback('leftclick', 'marksRoutesTracks', function (self) {
-        state = states.normal;
+        initState();
         self.button('toggle');
         $('.active-marksRoutesTracks').removeClass('active-marksRoutesTracks').addClass('inactive-marksRoutesTracks');
         $(self.data('name')).removeClass('inactive-marksRoutesTracks').addClass('active-marksRoutesTracks');
-        
+        /* set the search dialog with the latest searched keyword */
         lastSearch[active] = $('#search-marksRoutesTracks').val();
         active = self.data('name');
-        $('#search-marksRoutesTracks').val(lastSearch[active]);  
+        $('#search-marksRoutesTracks').val(lastSearch[active]);
+        /* be sure that the default footer is visible */
+        $('.menu-footer').removeClass('visible').addClass('hidden');
+        $('#menu-marksRoutesTracks-footer').removeClass('hidden').addClass('visible');
     });
     
     $("#search-marksRoutesTracks").keyup( function(event) {
@@ -104,7 +115,7 @@ $(document).ready(function() {
 
     /* handle leftclick events on a route */ 
     menu.addCallback('leftclick', ['icon-selectedRoute', 'icon-notSelectedRoute'], function (self) {
-        if(!map.checkTracking()) {
+        if(!map.checkTracking() && state == states.normal) {
             return;
         }
         if (state == states.normal && self.hasClass('icon-selectedRoute')) {
@@ -123,7 +134,7 @@ $(document).ready(function() {
 
     /* handle leftclick events on a track */ 
     menu.addCallback('leftclick', ['icon-selectedTrack', 'icon-notSelectedTrack'], function (self) {
-        if(!map.checkTracking()) {
+        if(!map.checkTracking() && state == states.normal) {
             return;
         }
         if (state == states.normal && self.hasClass('icon-selectedTrack')) {
