cc12fbf25c5afc5beb72d995da5f7afa3771dc8c
Patrick Kohan <patrick.kohan@googlemail.com>
2014-01-03 16:59:17 +0100

    changed setupdb script

--- setupDB/setupDB.sh
+++ setupDB/setupDB.sh
@@ -20,7 +20,7 @@ curl -X PUT http://$USERNAME:$PASSWORD@$HOST/seapal_person_db/
 curl -X PUT http://$USERNAME:$PASSWORD@$HOST/seapal_boats_db/_design/Boat -d @boat.json
 curl -X PUT http://$USERNAME:$PASSWORD@$HOST/seapal_mark_db/_design/Mark -d @mark.json
 curl -X PUT http://$USERNAME:$PASSWORD@$HOST/seapal_route_db/_design/Route -d @route.json
-curl -X PUT http://$USERNAME:$PASSWORD@$HOST/seapal_trips_db/_design/Trips -d @trips.json
+curl -X PUT http://$USERNAME:$PASSWORD@$HOST/seapal_trips_db/_design/Trip -d @trips.json
 curl -X PUT http://$USERNAME:$PASSWORD@$HOST/seapal_waypoint_db/_design/Waypoint -d @waypoint.json
 curl -X PUT http://$USERNAME:$PASSWORD@$HOST/seapal_person_db/_design/Person -d @person.json
 
