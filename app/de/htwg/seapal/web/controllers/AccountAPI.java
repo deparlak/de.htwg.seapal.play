4809d04eb6cd942e0324a4077f19b130f43c3f9c
Patrick Kohan <patrick.kohan@googlemail.com>
2014-01-13 10:57:22 +0100

    added openID login

--- app/de/htwg/seapal/web/controllers/AccountAPI.java
+++ app/de/htwg/seapal/web/controllers/AccountAPI.java
@@ -10,10 +10,13 @@ import de.htwg.seapal.web.controllers.helpers.Menus;
 import de.htwg.seapal.web.views.html.appContent.reset;
 import de.htwg.seapal.web.views.html.appContent.signInSeapal;
 import de.htwg.seapal.web.views.html.appContent.signUpSeapal;
+import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.node.ObjectNode;
 import play.data.DynamicForm;
 import play.data.Form;
+import play.libs.F;
 import play.libs.Json;
+import play.libs.OpenID;
 import play.mvc.Controller;
 import play.mvc.Http.Context;
 import play.mvc.Result;
@@ -22,6 +25,7 @@ import play.mvc.With;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -233,5 +237,26 @@ public class AccountAPI
 
             return unauthorized(response);
         }
+    }
+
+    ////// OpenID
+    public static Result auth() {
+        String providerUrl = "https://www.google.com/accounts/o8/id";
+        String returnToUrl = "http://localhost:9000/openID/verify";
+
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put("Email", "http://schema.openid.net/contact/email");
+        attributes.put("FirstName", "http://schema.openid.net/namePerson/first");
+        attributes.put("LastName", "http://schema.openid.net/namePerson/last");
+
+        F.Promise<String> redirectUrl = OpenID.redirectURL(providerUrl, returnToUrl, attributes);
+        return redirect(redirectUrl.get());
+    }
+
+    public static Result verify() {
+        F.Promise<OpenID.UserInfo> userInfoPromise = OpenID.verifiedId();
+        OpenID.UserInfo userInfo = userInfoPromise.get();
+        JsonNode json = Json.toJson(userInfo.attributes);
+        return ok(json);
     }
 }
