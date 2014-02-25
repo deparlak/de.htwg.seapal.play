7105816dbb35252f788ea73f2c580902154d9119
deparlak <deparlak@googlemail.com>
2014-02-26 00:01:34 +0100

    - added local buffer for logged out storage.

--- public/js/app/isLoggedOut.js
+++ public/js/app/isLoggedOut.js
@@ -9,17 +9,61 @@ $(document).ready(function() {
     events = map.getEvents();
     var id = 0;
     var rev = 0;
+    var store = {};
     
-    /* setup some example data entry's */
+    /* setup some example data. */
     tmp = map.getTemplate('person');
     tmp.firstname = 'Max';
     tmp.lastname = 'Mustermann';
+    tmp.owner = 'ACCOUNT_1';
+    tmp._id = 'SIMULATED_SERVER_ID_'+id++;
+    tmp._rev = 'SIMULATED_SERVER_REV_'+rev++;
     map.set('person', tmp);
     
     tmp = map.getTemplate('person');
     tmp.firstname = 'Friend';
     tmp.lastname = 'Account';
+    tmp.owner = 'ACCOUNT_2';
+    tmp._id = 'SIMULATED_SERVER_ID_'+id++;
+    tmp._rev = 'SIMULATED_SERVER_REV_'+rev++;
     map.set('person', tmp);
+    
+    store['ACCOUNT_1'] = {};
+    store['ACCOUNT_2'] = {};
+    
+    tmp = map.getTemplate('boat');
+    tmp.name = 'My Boat';
+    tmp.owner = 'ACCOUNT_2';
+    tmp._id = 'SIMULATED_SERVER_ID_'+id++;
+    tmp._rev = 'SIMULATED_SERVER_REV_'+rev++;
+    store['ACCOUNT_2'][tmp._id ] = tmp;
+    
+    tmp = map.getTemplate('trip');
+    tmp.name = 'My trip';
+    tmp.startDate = 1393354215906;
+    tmp.endDate = 1393354216906;
+    tmp.owner = 'ACCOUNT_2';
+    tmp._id = 'SIMULATED_SERVER_ID_'+id++;
+    tmp._rev = 'SIMULATED_SERVER_REV_'+rev++;
+    store['ACCOUNT_2'][tmp._id ] = tmp;
+   
+    tmp = map.getTemplate('mark');
+    tmp.name = 'Example mark 1';
+    tmp.lat = 47.7;
+    tmp.lng = 9.2;
+    tmp.owner = 'ACCOUNT_2';
+    tmp._id = 'SIMULATED_SERVER_ID_'+id++;
+    tmp._rev = 'SIMULATED_SERVER_REV_'+rev++;
+    store['ACCOUNT_2'][tmp._id ] = tmp;
+    
+    tmp = map.getTemplate('mark');
+    tmp.name = 'Example mark 2';
+    tmp.lat = 47.5;
+    tmp.lng = 9.5;
+    tmp.owner = 'ACCOUNT_2';
+    tmp._id = 'SIMULATED_SERVER_ID_'+id++;
+    tmp._rev = 'SIMULATED_SERVER_REV_'+rev++;
+    store['ACCOUNT_2'][tmp._id ] = tmp; 
     
 	menu.addCallback('leftclick', 'logbookCrewAdd', function (self) {
         output.warning("You are not logged in. Please log in to add a new crew member.");
@@ -32,6 +76,8 @@ $(document).ready(function() {
             self._rev = 'SIMULATED_SERVER_REV_'+rev++;
         }
         map.set(self.type, self);
+        self.id = null;
+        store[self.owner][self._id] = self;
     });
     
  	/* this callback will be called if an object was updated by a user */
@@ -42,16 +88,8 @@ $(document).ready(function() {
         $("#logbook-boats").html("");
         $("#marks").html("");
         
-        if('Friend' == self.firstname) {
-            tmp = map.getTemplate('boat');
-            tmp.name = 'My Boat';
-            map.set('boat', tmp);
-            
-            tmp = map.getTemplate('trip');
-            tmp.name = 'My trip';
-            tmp.startDate = 1393354215906;
-            tmp.endDate = 1393354216906;
-            map.set('trip', tmp); 
+		for (var tmp in store[self.owner]) {
+            map.set(store[self.owner][tmp].type, store[self.owner][tmp]);
         }
     });
 });
