afa13a5d38e9105b5f707070c55ca62f48b82f6c
lukaseckert <lueckert@htwg-konstanz.de>
2014-04-20 12:47:51 +0200

    extended logbook api

--- public/js/logbook/logbook.js
+++ public/js/logbook/logbook.js
@@ -1,6 +1,18 @@
+/**
+ * Get a range of pictures of a trip.
+ * Returns an array of (waypointId, thumbPicture) objects
+ * where thumbPicture can be assigned as src of <img> tags directly.
+ */	
 function getTripPhotos(tripId, startIndex, count, callback) {
 	$.getJSON('logbook/tripPhotos/' + tripId + '/' + startIndex + '/' + count)
 	.done(function (result) {
 		callback(tripId, result);
 	});
+}
+
+/**
+ * Returns the URL to the full-size picture of the specified waypoint.
+ */
+function getPhotoOfWaypoint(waypointId) {
+	return 'api/photo/' + waypointId + '/waypoint.jpg';
 }
