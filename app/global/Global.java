c2e7f6de5b84a0a9815b1346b91c88b6af964dcb
deparlak <deparlak@googlemail.com>
2014-07-24 15:53:37 +0200

    - updated Global.java to load modules depending on the conf/application.conf

--- app/global/Global.java
+++ app/global/Global.java
@@ -2,49 +2,52 @@ package global;
 
 import java.lang.reflect.Method;
 
-import global.module.*;
-
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
-import com.google.inject.Injector;
 
 import play.Logger;
 import play.Application;
 import play.GlobalSettings;
 import play.Logger.ALogger;
+import play.Play;
 import play.mvc.Action;
 import play.mvc.Http.Request;
 
 public class Global extends GlobalSettings {
-    private final static Injector INJECTOR = createInjector();
     private final ALogger logger = Logger.of("logger");
-    
-    private static Injector createInjector() {
-        return Guice.createInjector(new TestModule());
-    }
      
     @Override
     @SuppressWarnings("rawtypes")
     public Action onRequest(Request request, Method method) {
-    	System.out.println("Before every request");
     	logger.info("method=" + request.method() + " uri=" + request.uri() + " remote-address=" + request.remoteAddress());
-        
     	return super.onRequest(request, method);
     }
     
     @Override
     public <A> A getControllerInstance(Class<A> controllerClass) throws Exception {
-    	return INJECTOR.getInstance(controllerClass);
+    	logger.info("Inject Controller");
+    	String module = Play.application().configuration().getString("global.module");
+    	return Guice.createInjector((loadModule(module))).getInstance(controllerClass);
     }
 
     @Override
     public void onStart(Application app) {
-    	app.configuration().getClass("my.thing");
-    	Logger.info(value);
-    	Logger.info("Application has started");
+    	logger.info("Application has started");
     }
 
     @Override
     public void onStop(Application app) {
     	logger.info("Application has stopped");
+    }
+
+	private AbstractModule loadModule(String path) {
+		try {
+			AbstractModule module = (AbstractModule) Class.forName(path).newInstance();
+			logger.info("Loaded module : "+path);
+			return module;
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e1) {
+			logger.info("Loaded default module : global.module.DefaultModule");
+			return new global.module.DefaultModule();
+		}
     }
 }
